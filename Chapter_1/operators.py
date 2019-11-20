7dab9bf0c076770b1b12d68efec5b7d55b5397d3
Blue3000 <kulicivan543@gmail.com>
2019-11-20 16:39:58 +0100

    Chapter 1 - operators, data types

---
+++ Chapter_1/operators.py
@@ -0,0 +1,52 @@
+## Math Operators from Highest to Lowest Precedendce
+
+# Exponent (**)
+2 ** 3 # 8
+
+# Modulus/Remainder (%)
+22 % 8 # 6
+
+# Integer division (//)
+22 // 8 # 2
+
+# Division (/)
+22 / 8 # 2.75
+
+# Multiplication (*)
+3 * 5 # 15
+
+# Subtraction (-)
+5 - 2 # 3
+
+# Addition (+)
+2 + 2 # 4
+
+
+
+## Common Data Types
+
+# Integers
+integers = [-2, -1, 0, 1, 2, 3]
+
+# Float-point numbers
+floats = [-1.25, -1.0,0.5]
+
+# Strings 
+string = ['aa', 'aaa', 'Hello !', '12 Cats']
+
+
+## Examples
+
+'Alice' * 5
+
+
+## Valid and Invalid Variable Names
+
+valid_variable_names = [
+    balacnce, currentBalance, current_balance, _spam, SPAM, abc4
+]
+
+invalid_variable_names = [
+    current-balance, current balance, 4account, 42, total_$um
+]
+
